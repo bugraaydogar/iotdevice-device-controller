ea533df7cd6c4c4669e709203460f96611a7adc9
Buğra Aydoğar <bugra.aydogar@canonical.com>
2022-05-23 22:25:43 +0300

    Use snapd rest api to refresh and revert snaps

--- device-controller/app/main.py
+++ device-controller/app/main.py
@@ -1,8 +1,50 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+
+from .snapd import SnapdClient
 
 app = FastAPI()
+snap_client = SnapdClient()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/ping")
 def pong():
     return {"ping": "pong!"}
+
+
+@app.get("/battery-level")
+def batteryLevel():
+    return {"battery": 50}
+
+
+@app.get("/system-info")
+def system_info():
+    return snap_client.snap_system_info()
+
+
+@app.post("/refresh")
+def refresh():
+    response = snap_client.refresh()
+    return response
+
+
+@app.post("/revert")
+def revert(snaps: List[str]):
+    response = snap_client.revert(snaps)
+    return response
+
