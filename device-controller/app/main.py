658f12bfed9657f51b6007cc0080bbdf4f3e45f0
Buğra Aydoğar <bugra.aydogar@canonical.com>
2023-11-17 13:40:41 +0300

    Added a new version of the API

--- device-controller/app/main.py
+++ device-controller/app/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI, APIRouter
 
 
-BATTERY_LEVEL = 55
+BATTERY_LEVEL = 90
+BATTERY_LEVEL_MAX = 100
 SPEED_LEVEL = 90
+SPEED_LEVEL_MAX = 100
 ANGULAR_FREEDOM = 245
-VERSION_V1 = 1.0
+ANGULAR_FREEDOM_ROTATION = "left"
+VERSION_V1 = 2.0
 
 app = FastAPI()
 
 version_v1 = APIRouter()
+version_v2 = APIRouter()
 
 # Common API
 @app.get("/version")
@@ -17,7 +21,12 @@ async def get_api_version():
 
 # Features API
 @version_v1.get("/features")
-async def get_features_list():
+async def get_features_list_v1():
+    return {"features": ["speed", "batterylevel", "angularfreedom"]}
+
+
+@version_v2.get("/features")
+async def get_features_list_v2():
     return {"features": ["speed", "batterylevel", "angularfreedom"]}
 
 
@@ -26,15 +35,29 @@ async def get_features_list():
 async def get_battery_level_v1():
     return {"level": BATTERY_LEVEL}
 
+@version_v2.get("/batterylevel")
+async def get_battery_level_v2():
+    return {"level": BATTERY_LEVEL, "max_capacity": BATTERY_LEVEL_MAX}
+
+
 # Speed API
 @version_v1.get("/speed")
 async def get_speed_level_v1():
     return {"speed": SPEED_LEVEL}
+
+@version_v2.get("/speed")
+async def get_speed_level_v2():
+    return {"speed": SPEED_LEVEL, "max_speed": SPEED_LEVEL_MAX}
 
 # Robotic API
 @version_v1.get("/angularfreedom")
 async def get_angularfreedom_v1():
     return {"angle": ANGULAR_FREEDOM}
 
+@version_v2.get("/angularfreedom")
+async def get_angularfreedom_v2():
+    return {"angle": ANGULAR_FREEDOM, "rotation": ANGULAR_FREEDOM_ROTATION}
+
 
 app.include_router(version_v1, prefix='/v1')
+app.include_router(version_v2, prefix='/v2')
